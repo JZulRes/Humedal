76c3766a72d27e6004bf1d7062f9664b0264fa30
Juan Fer <JuanFer@macbook-pro-de-juanfer.local>
2016-06-22 09:44:50 -0500

    Initial Commit

---
+++ HumedalTests/HumedalTests.swift
@@ -0,0 +1,36 @@
+//
+//  HumedalTests.swift
+//  HumedalTests
+//
+//  Created by JuanFer Zuluaga on 22/06/16.
+//  Copyright © 2016 JuanFer Zuluaga. All rights reserved.
+//
+
+import XCTest
+@testable import Humedal
+
+class HumedalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
